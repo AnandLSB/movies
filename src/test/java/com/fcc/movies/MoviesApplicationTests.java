69fc1a75eb6ba0496b99035a5606631f351ac2ee
Anand <kuantum28@gmail.com>
2024-06-14 13:30:05 +0800

    first-commit

---
+++ src/test/java/com/fcc/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fcc.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
